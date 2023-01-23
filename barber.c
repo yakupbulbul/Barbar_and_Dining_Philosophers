91cc35ef5c7920f5a0af4ce39b1a811cca8b2415
yakupbulbul <81179438+yakupbulbul@users.noreply.github.cod>
2023-01-23 16:26:41 +0300

    some basics changes

--- barber.c
+++ barber.c
@@ -5,45 +5,55 @@
 #include <errno.h>
 
 #define MAX_CUSTOMERS 27
+
 #define MAX_WAITING_CHAIRS 5
 
-// Mutex and condition variable to synchronize access to the buffer
+// Mutex and condition variable to synchronization of access to the buffer
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 pthread_cond_t barber_sleep = PTHREAD_COND_INITIALIZER;
+
 pthread_cond_t customer_done = PTHREAD_COND_INITIALIZER;
 
-// Struct to represent a customer
+// Struct to representing of a customer
 struct customer {
     int id;
     time_t arrive_time;
 };
 
-// Buffer (waiting chairs) implemented as a circular queue
+// Buffer (waiting chairs) implementation as a circular queue
 struct customer waiting_customers[MAX_WAITING_CHAIRS];
+
 int head = 0, tail = 0, waiting_count = 0;
 
-// Function to add a customer to the buffer
+// Function to adding a customer to the buffer
 void add_customer_to_buffer(int id, time_t arrive_time) {
+
     waiting_customers[tail].id = id;
+
     waiting_customers[tail].arrive_time = arrive_time;
     tail = (tail + 1) % MAX_WAITING_CHAIRS;
     waiting_count++;
 }
 
-// Function to get the next customer from the buffer
+// Function to geting the next customer from the buffer
 struct customer get_next_customer() {
     struct customer next = waiting_customers[head];
+
+
     head = (head + 1) % MAX_WAITING_CHAIRS;
+
+
     waiting_count--;
     return next;
 }
 
-// Function to check if the buffer is empty
+// Function to checking if the buffer is empty
 int buffer_is_empty() {
     return waiting_count == 0;
 }
 
-// Function to check if the buffer is full
+// Function to checking if the buffer is full
 int buffer_is_full() {
     return waiting_count == MAX_WAITING_CHAIRS;
 }
@@ -51,85 +61,125 @@ int buffer_is_full() {
 // Function for the barber thread
 void* barber_thread(void* arg) {
     while (1) {
+
         pthread_mutex_lock(&mutex);
+
         while (buffer_is_empty()) {
             printf("Barber: sleeping\n");
             pthread_cond_wait(&barber_sleep, &mutex);
         }
         struct customer next_customer = get_next_customer();
+
         pthread_mutex_unlock(&mutex);
 
-        printf("Barber: cutting hair of customer %d\n", next_customer.id);
+        printf("Barber: cutting hair of Customer %d\n", next_customer.id);
+
         struct timespec duration;
+
         duration.tv_sec = 0;
+
         duration.tv_nsec = (*((int*) arg)) * 1000000;
+
         nanosleep(&duration, NULL);
+
         pthread_cond_signal(&customer_done);
+
     }
     return NULL;
 }
 
 // Function for the customer thread
 void* customer_thread(void* arg) {
-    int id = *((int*) arg);
+
+       int id = *((int*) arg);
     time_t arrive_time;
     int haircuts = *((int*) (arg + sizeof(int)));
     int wait_time;
+
+
     while (haircuts--) {
         pthread_mutex_lock(&mutex);
         arrive_time = time(NULL);
+
+
         if (buffer_is_full()) {
-            printf("Customer %d: leaving\n", id);
+            printf("Customer %d: Leaving\n", id);
             pthread_mutex_unlock(&mutex);
+
+
             struct timespec duration;
             duration.tv_sec = 0;
+
+
             duration.tv_nsec = (*((int*) (arg + sizeof(int) * 2))) * 1000000;
+
+
             nanosleep(&duration, NULL);
             continue;
         }
         add_customer_to_buffer(id, arrive_time);
-        printf("Customer %d: entering shop, waiting\n", id);
+        printf("Customer %d: Entering shop, waiting\n", id);
+
         pthread_cond_signal(&barber_sleep);
         pthread_cond_wait(&customer_done, &mutex);
+
+
         wait_time = difftime(time(NULL), arrive_time);
-        printf("Customer %d: waited %d seconds\n", id, wait_time);
+
+
+        printf("Customer %d: Waited %d seconds\n", id, wait_time);
+
+
         pthread_mutex_unlock(&mutex);
         struct timespec duration;
         duration.tv_sec = 0;
+
         duration.tv_nsec = (*((int*) (arg + sizeof(int) * 2))) * 1000000;
+        
         nanosleep(&duration, NULL);
     }
     return NULL;
 }
 
 int main(int argc, char* argv[]) {
+
     if (argc < 5) {
-        printf("Usage: barber <num_customers> <max_arrival_time> <max_haircut_duration> <haircut_repetition>\n");
+        fprintf(stderr, "Usage: barber <num_customer> <max_arrival_time> <max_haricut_duration> <haricut_repetion>\n");
         return 1;
     }
+    int num_of_customers = atoi(argv[1]);
 
-    int num_customers = atoi(argv[1]);
+
     int max_arrival_time = atoi(argv[2]);
+
     int max_haircut_duration = atoi(argv[3]);
+
     int haircut_repetition = atoi(argv[4]);
 
-    pthread_t barber;
-    int i;
-    pthread_t customers[num_customers];
-    int customer_args[num_customers][3];
+    if (num_of_customers > MAX_CUSTOMERS) {
+        fprintf(stderr, "Error: Max number of customers is %d\n", MAX_CUSTOMERS);
+        return 1;
+    }
+    int customer_args[num_of_customers][3];
+
+
+    pthread_t barber, customers[num_of_customers];
 
     pthread_create(&barber, NULL, barber_thread, &max_haircut_duration);
-    for (i = 0; i < num_customers; i++) {
-        customer_args[i][0] = i;
+    for (int i = 0; i < num_of_customers; i++) {
+
+        customer_args[i][0] = i + 1;
+
         customer_args[i][1] = haircut_repetition;
+
         customer_args[i][2] = max_arrival_time;
         pthread_create(&customers[i], NULL, customer_thread, customer_args[i]);
     }
+    for (int i = 0; i < num_of_customers; i++) {
 
-    for (i = 0; i < num_customers; i++) {
+
         pthread_join(customers[i], NULL);
     }
-    pthread_join(barber, NULL);
-
     return 0;
 }
+
--- phsp.c
+++ phsp.c
@@ -8,12 +8,16 @@
 #include <inttypes.h>
 
 #define MIN_THINK 1
-#define MAX_THINK 60000
+#define MAX_THINK 100000
 #define MIN_DINE 1
-#define MAX_DINE 60000
+#define MAX_DINE 100000
 
-int num_philosophers;
+
+
+int number_of_philosophers;
+
 int min_think, max_think;
+
 int min_dine, max_dine;
 char* dst;
 int num_dine;
@@ -25,17 +29,21 @@ double rand_exp(double mean) {
     return -mean * log(u);
 }
 
+
+
 void *philosopher(void *arg) {
     int id = (intptr_t)arg;
     int left = id;
-    int right = (id + 1) % num_philosophers;
+    int right = (id + 1) % number_of_philosophers;
     int i;
     for (i = 0; i < num_dine; i++) {
         int thinking_time;
+        // dst =="uniform"
         if (strcmp(dst
     , "uniform") == 0) {
             thinking_time = rand() % (max_think - min_think + 1) + min_think;
         } else if (strcmp(dst, "exponential") == 0) {
+            // dst =="uniform"
             double mean = (min_think + max_think) / 2.0;
             thinking_time = (int)rand_exp(mean);
             while (thinking_time < min_think || thinking_time > max_think) {
@@ -44,18 +52,29 @@ void *philosopher(void *arg) {
         }
 
         struct timespec sleep_time;
+
+        //timespec time 
+
+
+    
         sleep_time.tv_sec = thinking_time/1000;
+
         sleep_time.tv_nsec = (thinking_time % 1000) * 1000000;
+
         nanosleep(&sleep_time, NULL);
 
         // Start measuring hungry time
         clock_t start = clock();
+
         pthread_mutex_lock(&chopsticks[left]);
+
         pthread_mutex_lock(&chopsticks[right]);
 
         int dining_time;
+        // dst =="uniform"
         if (strcmp(dst, "uniform") == 0) {
             dining_time = rand() % (max_dine - min_dine + 1) + min_dine;
+            // dst =="exponential"
         } else if (strcmp(dst, "exponential") == 0) {
             double mean = (min_dine + max_dine) / 2.0;
             dining_time = (int)rand_exp(mean);
@@ -65,15 +84,20 @@ void *philosopher(void *arg) {
         }
 
         sleep_time.tv_sec = dining_time/1000;
+
+
         sleep_time.tv_nsec = (dining_time % 1000) * 1000000;
         nanosleep(&sleep_time, NULL);
 
         pthread_mutex_unlock(&chopsticks[left]);
+
+
         pthread_mutex_unlock(&chopsticks[right]);
 
         // Stop measuring hungry time
         clock_t end = clock();
         double duration = (double)(end - start) / CLOCKS_PER_SEC;
+
         hungry_time[id] += duration;
     }
     return 0;
@@ -85,53 +109,69 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Read input from command line
-    num_philosophers = atoi(argv[1]);
+    // It can read input
+    number_of_philosophers = atoi(argv[1]);
+
     min_think = atoi(argv[2]);
+    
     max_think = atoi(argv[3]);
     min_dine = atoi(argv[4]);
+
     max_dine = atoi(argv[5]);
     dst = argv[6];
+
     num_dine = atoi(argv[7]);
 
-    // Initialize chopsticks
+
+
+    // Initialization of chopsticks
     int i;
-    for (i = 0; i < num_philosophers; i++) {
+    for (i = 0; i < number_of_philosophers; i++) {
         pthread_mutex_init(&chopsticks[i], NULL);
     }
 
-    // Create threads for philosophers
-    pthread_t philosophers[num_philosophers];
-    for (i = 0; i < num_philosophers; i++) {
+    // Creating of threads for philosophers
+    pthread_t philosophers[number_of_philosophers];
+    for (i = 0; i < number_of_philosophers; i++) {
+
         pthread_create(&philosophers[i], NULL, philosopher, (void *)(intptr_t)i);
     }
 
-    // Wait for all philosophers to finish dining
-    for (i = 0; i < num_philosophers; i++) {
+    // Waiting for all philosophers to finish dining
+    for (i = 0; i < number_of_philosophers; i++) {
+
         pthread_join(philosophers[i], NULL);
     }
 
-    // Calculate average hungry time
+    // Calculate mean of hungry time
     double total_hungry_time = 0;
-    for (i = 0; i < num_philosophers; i++) {
+    for (i = 0; i < number_of_philosophers; i++) {
+
         total_hungry_time += hungry_time[i];
     }
-    double average_hungry_time = total_hungry_time / num_philosophers;
+    double average_hungry_time = total_hungry_time / number_of_philosophers;
+
+
 
     // Calculate standard deviation of hungry time
     double sum_of_squared_differences = 0;
-    for (i = 0; i < num_philosophers; i++) {
+
+    for (i = 0; i < number_of_philosophers; i++) {
         double difference = hungry_time[i] - average_hungry_time;
+
         sum_of_squared_differences += difference * difference;
     }
-    double standard_deviation = sqrt(sum_of_squared_differences / num_philosophers);
+    double standard_deviation = sqrt(sum_of_squared_differences / number_of_philosophers);
+
+
 
     // Print results
-    printf("Average hungry time: %f\n", average_hungry_time);
+    printf("Mean time of hungry: %f\n", average_hungry_time);
+
     printf("Standard deviation of hungry time: %f\n", standard_deviation);
 
     // Clean up chopsticks
-    for (i = 0; i < num_philosophers; i++) {
+    for (i = 0; i < number_of_philosophers; i++) {
         pthread_mutex_destroy(&chopsticks[i]);
     }
 
